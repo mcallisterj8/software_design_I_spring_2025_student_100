b1e1c78e00e4832a64bd9e0c6451258fe370afae
Jacob McAllister <mcallisterj@westerntc.edu>
2025-03-03 10:10:58 -0600

    End of class
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

---
+++ Intro/Program.cs
@@ -0,0 +1,12 @@
+﻿// List<int> numbers = new List<int>();
+
+
+List<int> numbers = 
+    new List<int> { 2, 5, 6, 8 };
+
+foreach(int elem in numbers){
+    Console.WriteLine($"elem: {elem}");
+}
+
+
+numbers.Add(9);
